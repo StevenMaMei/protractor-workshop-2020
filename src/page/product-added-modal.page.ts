792431c69f643d34fb8df083388c56a598ec0312
StevenMaMei <33093117+StevenMaMei@users.noreply.github.com>
2020-07-28 16:53:41 -0500

    Using only explicit waits (#13)
    * explicit waitings added and implicit waiting removed. I had to add only 2 explicit waits
    
    * changes done
    
    * import changes

--- src/page/product-added-modal.page.ts
+++ src/page/product-added-modal.page.ts
@@ -1,13 +1,17 @@
-import { $, ElementFinder } from 'protractor';
+import { $, ElementFinder, browser, ExpectedConditions } from 'protractor';
 
 export class ProductAddedPage {
   private addToCartBtn: ElementFinder;
+  private timeout: number = 5000;
 
   constructor () {
     this.addToCartBtn = $('#center_column a.button.ajax_add_to_cart_button.btn.btn-default');
   }
 
   public async addProduct(): Promise<void> {
+    await browser.wait(
+      ExpectedConditions.visibilityOf(this.addToCartBtn),
+      this.timeout);
     await this.addToCartBtn.click();
   }
 }
--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,13 +1,17 @@
-import { $, ElementFinder } from 'protractor';
+import { $, ElementFinder, browser, ExpectedConditions } from 'protractor';
 
 export class ProductListPage {
   private proceedCheckoutBtn: ElementFinder;
+  private timeout: number = 5000;
 
   constructor () {
     this.proceedCheckoutBtn = $('[style*="display: block;"] .button-container > a');
   }
 
   public async proceedToCheckOut(): Promise<void> {
+    await browser.wait(
+      ExpectedConditions.visibilityOf(this.proceedCheckoutBtn),
+      this.timeout);
     await this.proceedCheckoutBtn.click();
   }
 }
