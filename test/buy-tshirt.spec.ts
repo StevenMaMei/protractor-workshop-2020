22b84c65b4c09e6f110e6965c3b642d237d655ff
StevenMaMei <33093117+StevenMaMei@users.noreply.github.com>
2020-07-29 15:38:03 -0500

    separating the test into many describes

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -2,24 +2,21 @@ import { browser } from 'protractor';
 import { MenuContentPage, SignInPage, SummaryStepPage, AddresStepPage, ShippingStepPage,
   PaymentStepPage, BankPaymentPage, OrderSummaryPage, ProductAddedPage, ProductListPage } from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  const email: string = 'aperdomobo@gmail.com';
-  const password: string = 'WorkshopProtractor';
+describe('Open the website', () => {
 
+  it('then the website should be opened', async() => {
+    await browser.get('http://automationpractice.com/');
+  });
+
+});
+
+describe('Buy a T-shirt', () => {
   const menuContentPage: MenuContentPage = new MenuContentPage();
   const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const signInPage: SignInPage = new SignInPage(email, password);
-  const addresStepPage: AddresStepPage = new AddresStepPage();
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
   const productAddedPage: ProductAddedPage = new ProductAddedPage();
   const productListPage: ProductListPage = new ProductListPage();
 
-  it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
-
+  it('then it should select the t-shirt and buy it', async() => {
     await menuContentPage.goToTShirtMenu();
 
     await productListPage.addProduct();
@@ -27,15 +24,40 @@ describe('Buy a t-shirt', () => {
     await productAddedPage.proceedToCheckOut();
 
     await summaryStepPage.completeSummaryStep();
+  });
 
+});
+
+describe('Log-in', () => {
+  const email: string = 'aperdomobo@gmail.com';
+  const password: string = 'WorkshopProtractor';
+
+  const signInPage: SignInPage = new SignInPage(email, password);
+
+  it('then you should be logged', async () => {
     await signInPage.completeSignInStep();
+  });
+});
 
+describe('Select the default address and accept the shipping', () => {
+  const addresStepPage: AddresStepPage = new AddresStepPage();
+  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+
+  it('then you should select the defauld addres and accept the terms of shipping', async() => {
     await addresStepPage.completeAddresStepPage();
 
     await shippingStepPage.agreeTermsShippingStep();
 
     await shippingStepPage.completeShippingStep();
+  });
+});
 
+describe('Proceed to pay', () => {
+  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+
+  it('then you should have paid and the purchase must be succesful', async() => {
     await paymentStepPage.goToPayByBank();
 
     await bankPaymentPage.confirmOrder();
@@ -43,4 +65,5 @@ describe('Buy a t-shirt', () => {
     await expect(orderSummaryPage.getConfirmationText())
       .toBe('Your order on My Store is complete.');
   });
+
 });
