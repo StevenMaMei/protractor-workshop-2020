ad543e3d109fb23a2105fe5aa5ccb1272e5af865
StevenMaMei <33093117+StevenMaMei@users.noreply.github.com>
2020-07-29 19:06:29 -0500

    multiples it removed and describe messages improved

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -2,68 +2,60 @@ import { browser } from 'protractor';
 import { MenuContentPage, SignInPage, SummaryStepPage, AddresStepPage, ShippingStepPage,
   PaymentStepPage, BankPaymentPage, OrderSummaryPage, ProductAddedPage, ProductListPage } from '../src/page';
 
-describe('Open the website', () => {
+describe('The user enters to the website', () => {
 
-  it('then the website should be opened', async() => {
+  beforeAll(async() => {
     await browser.get('http://automationpractice.com/');
   });
+  describe('he goes to the t-shirt menu and adds one t-shirt to the cart', () => {
+    beforeAll(async () => {
+      const menuContentPage: MenuContentPage = new MenuContentPage();
+      const summaryStepPage: SummaryStepPage = new SummaryStepPage();
+      const productAddedPage: ProductAddedPage = new ProductAddedPage();
+      const productListPage: ProductListPage = new ProductListPage();
 
-});
+      await menuContentPage.goToTShirtMenu();
 
-describe('Buy a T-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const productAddedPage: ProductAddedPage = new ProductAddedPage();
-  const productListPage: ProductListPage = new ProductListPage();
+      await productListPage.addProduct();
 
-  it('then it should select the t-shirt and buy it', async() => {
-    await menuContentPage.goToTShirtMenu();
+      await productAddedPage.proceedToCheckOut();
 
-    await productListPage.addProduct();
+      await summaryStepPage.completeSummaryStep();
+    });
+    describe('He logs into the system', () => {
+      beforeAll(async () => {
+        const email: string = 'aperdomobo@gmail.com';
+        const password: string = 'WorkshopProtractor';
+        const signInPage: SignInPage = new SignInPage(email, password);
 
-    await productAddedPage.proceedToCheckOut();
+        await signInPage.completeSignInStep();
+      });
+      describe('he selects the default address and accepts the shipping terms', () => {
+        beforeAll(async () => {
+          const addresStepPage: AddresStepPage = new AddresStepPage();
+          const shippingStepPage: ShippingStepPage = new ShippingStepPage();
 
-    await summaryStepPage.completeSummaryStep();
+          await addresStepPage.completeAddresStepPage();
+
+          await shippingStepPage.agreeTermsShippingStep();
+
+          await shippingStepPage.completeShippingStep();
+        });
+        describe('He proceeds to pay', () => {
+          beforeAll(async () => {
+            const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+            const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+            await paymentStepPage.goToPayByBank();
+            await bankPaymentPage.confirmOrder();
+          });
+
+          it('the purchase has been completed', async() => {
+            const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+            await expect(orderSummaryPage.getConfirmationText())
+              .toBe('Your order on My Store is complete.');
+          });
+        });
+      });
+    });
   });
-
-});
-
-describe('Log-in', () => {
-  const email: string = 'aperdomobo@gmail.com';
-  const password: string = 'WorkshopProtractor';
-
-  const signInPage: SignInPage = new SignInPage(email, password);
-
-  it('then you should be logged', async () => {
-    await signInPage.completeSignInStep();
-  });
-});
-
-describe('Select the default address and accept the shipping', () => {
-  const addresStepPage: AddresStepPage = new AddresStepPage();
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-
-  it('then you should select the defauld addres and accept the terms of shipping', async() => {
-    await addresStepPage.completeAddresStepPage();
-
-    await shippingStepPage.agreeTermsShippingStep();
-
-    await shippingStepPage.completeShippingStep();
-  });
-});
-
-describe('Proceed to pay', () => {
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
-
-  it('then you should have paid and the purchase must be succesful', async() => {
-    await paymentStepPage.goToPayByBank();
-
-    await bankPaymentPage.confirmOrder();
-
-    await expect(orderSummaryPage.getConfirmationText())
-      .toBe('Your order on My Store is complete.');
-  });
-
 });
